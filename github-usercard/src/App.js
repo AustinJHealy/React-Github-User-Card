029741c4dc91b56be966a3a20b3898b726e0e805
AustinJHealy <56621614+AustinJHealy@users.noreply.github.com>
2020-06-09 16:39:23 -0400

    created react app and installed dependencies

---
+++ github-usercard/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import CardContainer from './components/CardContainer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <CardContainer />
+    </div>
+  );
+}
+
+export default App;
